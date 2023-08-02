e5783420015dc1f77ba415a74bfa86b934fbc4d2
Your Name <nicemantim@163.com>
2023-08-02 14:24:38 -0400

    manage

---
+++ AH_Lawn_Mower_Base_Design_ESP32_Blade.ino
@@ -0,0 +1,376 @@
+/**
+On power-on, the esp will either: 
+1) produce a AP hotspot 'Willa_Mower' for user login with password ‘BobdaSML' on the user's device.
+  The user can then configure a new network for the mower to use by visiting 192.168.4.1 in the user's browser.
+or If the network has previously been configured and is avaliable
+2) the mower will connect to the previously used network.
+Finally, the mower can be remotely controlled by the user's device by the given ip address.
+*/
+#include <WiFiClient.h>
+#include <HardwareSerial.h>
+// #include <ESPmDNS.h>
+
+#include <WebServer.h>
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+
+
+// #include <WiFi.h>
+// select which pin will trigger the configuration portal when set to LOW
+
+int timeout = 120; // seconds to run for
+
+//const int rc0 = 5; // GPIO pin for RC channel 2
+//const int rc1 = 12; // GPIO pin for RC channel 6
+//const int relay3 = 13; // GPIO pin for drive motor relay 
+
+
+// radio inputs
+// const int RC_CH2_PIN = 4;   // RC channel 2 (steering) D4
+// const int RC_CH6_PIN = 21;  // RC channel 6 (throttle) D21 , could use 25,26,27
+// Relay pin
+const int MOTOR_PIN = 22;      // Motor on/off control D22 aka relay3
+// const int MOTOR_DIR1_PIN = 23; // Motor direction control 1 (FB HL) D23
+const int ACTUATOR_PIN1 = 16;   // Actuator control Extend RX2
+const int ACTUATOR_PIN2 = 17;   // Actuator control Retract TX2
+
+const int BLADE_PIN =23;   
+
+int stateForBlade = 0;
+
+#define TRIGGER_PIN 18 // D18 For reset WiFi credential
+#define TX_PIN 32
+#define RX_PIN 33
+
+
+
+WebServer server(80);
+
+enum motorState {
+  STOPPED,  FORWARD,  BACKWARD,  LEFT,  RIGHT
+};
+
+volatile int throttle = 0; // RC channel 6 (throttle)
+volatile int steering = 0; // RC channel 2 (steering)
+int motorState = STOPPED;
+
+// Define pulse width ranges for RC receiver channels
+const int RC_CH2_RIGHT_MIN = 1100;
+const int RC_CH2_RIGHT_MAX = 1300;
+const int RC_CH2_LEFT_MIN = 1700;
+const int RC_CH2_LEFT_MAX = 1900;
+const int RC_CH6_FORWARD_MIN = 1100; 
+const int RC_CH6_FORWARD_MAX = 1300;
+const int RC_CH6_BACKWARD_MIN = 1700;
+const int RC_CH6_BACKWARD_MAX = 1900;
+
+// HTML and CSS for the web page
+// String html = "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><style>body {font-family: Arial; margin: 0;}"
+//               "/* Style the header */.header {padding: 60px; text-align: center; background: #1abc9c; color: white;}"
+//               "/* Style the buttons */.button {background-color: #008CBA; border: none; color: white; padding: 16px 32px; text-align: center; text-decoration: none;"
+//               "display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;}.button2 {background-color: #f44336;}"
+//               ".button-container {text-align: center;}"
+//               "#stream-container {text-align: center;}"
+//               "#stream {max-width: 100%; height: auto; display: block; margin: auto;}"
+//               "</style></head><body>"
+//               "<div class='header'><h1>Remote Control Lawn Mower</h1><p>Control your lawn mower from anywhere</p></div>"
+//               "<div id='stream-container'>"
+//               "<img id='stream' src='http://192.168.168.248:81/stream'>"
+//               "</div>"
+//               "<div style='padding:20px' class='button-container'><a href='/forward'><button class='button'>Forward</button></a></div>"
+//               "<div style='padding:20px' class='button-container'><a href='/left'><button class='button'>Left</button></a>"
+//               "<a href='/stop'><button class='button button2'>Stop</button></a>"
+//               "<a href='/right'><button class='button'>Right</button></a></div><div style='padding:20px' class='button-container'>"
+//               "<a href='/backward'><button class='button'>Backward</button></a></div>"
+//               "</body></html>";    
+
+
+String html = "<!DOCTYPE html> <html>  <head> <meta name='viewport' content='width=device-width, initial-scale=1'> <style> /* Your existing CSS styles */ body { font-family: Arial; margin: 0; }  /* Style the header */ .header { padding: 60px; text-align: center; background: #1abc9c; color: white; }  /* Style the buttons */ .button { background-color: #008CBA; border: none; color: white; padding: 16px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; }  .button2 { background-color: #f44336; }  .button-container { text-align: center; } #mower-state {color: white;padding: 60px;text-align: center; background: #1abc9c;} #stream-container { text-align: center; }   #stream { max-width: 100%; height: auto; display: block; margin: auto; } </style> </head>  <body> <div class='header'> <h1>Remote Control Lawn Mower</h1> <p>Control your lawn mower from anywhere</p> </div>";    ; 
+              
+// String htmlWithIP = "<img id='stream' src='http://192.168.168.248:81/stream'>";
+String htmlWithIP = "<img id='stream' src='http://";
+String htmlWithIPRemain;
+String html1 ="</div>"
+              "<div style='padding:20px' class='button-container'> <button class='button' onclick='sendCommand(\"forward\");'>Forward</button> </div> <div style='padding:20px' class='button-container'> <button class='button' onclick='sendCommand(\"left\");'>Left</button> <button class='button button2' onclick='sendCommand(\"stop\");'>Stop</button> <button class='button' onclick='sendCommand(\"right\");'>Right</button> </div> <div style='padding:20px' class='button-container'> <button class='button' onclick='sendCommand(\"backward\");'>Backward</button> </div> <!-- Display the mower state here --> <div style='padding:20px' class='button-container'> <button class='button' onclick='sendCommand(\"bladeon\");'>Blade On</button> <button class='button button2' onclick='sendCommand(\"bladeoff\");'>Blade Off</button> </div><div id='mower-state'><p>Mower State: </p></div><script> function sendCommand(command){ fetch('/' + command, { method: 'POST', }).then(response => { if (!response.ok) { throw new Error('Network response was not ok'); } console.log('Command sent successfully:', command); history.pushState({}, null, '/' + command); if (command === 'right') { updateMowerState('Right'); } if (command === 'left') { updateMowerState('Left'); } if (command === 'backward') { updateMowerState('Backward'); } if (command === 'forward') { updateMowerState('Forward'); } if (command === 'stop') { updateMowerState('Stop'); } }).catch(error => { console.error('Error sending command:', error); }); } function updateMowerState(state) { var mowerStateElement = document.getElementById('mower-state'); mowerStateElement.innerHTML = \"<p>Mower State: \" + state + \"</p>\"; } </script></body></html>"; 
+
+String webPageHTML;
+
+
+void handleRoot() {
+  server.send(200, "text/html", webPageHTML);
+}
+void handleForward() {
+  motorState = FORWARD;
+  server.send(200, "text/html", "<div class='header'><p>Mower State: Forward</p></div>");
+}
+void handleBackward() {
+  motorState = BACKWARD;
+  server.send(200, "text/html",  "<div class='header'><p>Mower State: Backward</p></div>");
+}
+void handleLeft() {
+  motorState = LEFT;
+  server.send(200, "text/html", "<div class='header'><p>Mower State: Left</p></div>");
+}
+void handleRight() {
+  motorState = RIGHT;
+  server.send(200, "text/html",  "<div class='header'><p>Mower State: Right</p></div>");
+}
+void handleStop() {
+  motorState = STOPPED;
+  server.send(200, "text/html", "<div class='header'><p>Mower State: Stopped</p></div>");
+}
+
+void handleBladeOn() {
+  stateForBlade = 1;
+  Serial.println("The Blade is on");
+  server.send(200, "text/html", "<div class='header'><p>Mower State: bladeon</p></div>");
+}
+
+void handleBladeOff() {
+  stateForBlade = 0;
+  Serial.println("The Blade is off");
+  server.send(200, "text/html", "<div class='header'><p>Mower State: bladeoff</p></div>");
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found");
+}
+
+// Handle motor control path request
+void handleMotor() {
+  String state = server.arg("state");
+  if (state == "forward") {
+    motorState = FORWARD;
+  } else if (state == "backward") {
+    motorState = BACKWARD;
+  } else if (state == "left") {
+    motorState = LEFT;
+  } else if (state == "right") {
+    motorState = RIGHT;
+  } else if (state == "stop") {
+    motorState = STOPPED;
+  }
+  server.sendHeader("Location", "/");
+  server.send(302, "text/plain", "");
+}
+
+// Handle actuator control path request
+void handleActuator() {
+  String state = server.arg("state");
+  if (state == "extend") {
+    digitalWrite(ACTUATOR_PIN1, HIGH);
+    digitalWrite(ACTUATOR_PIN2, LOW);
+  } else if (state == "retract") {
+    digitalWrite(ACTUATOR_PIN1, LOW);
+    digitalWrite(ACTUATOR_PIN2, HIGH);
+  }
+  server.sendHeader("Location", "/");
+  server.send(302, "text/plain", "");
+}
+
+void setup() {
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP  
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  // espSerial.begin(9600);
+  Serial.println("\n Starting");
+  Serial1.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);
+  pinMode(TRIGGER_PIN, INPUT_PULLUP);
+  WiFiManager wm;
+ 
+
+
+bool res;
+ 
+if (digitalRead(TRIGGER_PIN) == HIGH){
+  res = wm.autoConnect("Willa_Mower","BobdaSML"); // password protected ap
+ 
+ 
+ 
+ 
+    if(!res) {
+ 
+        Serial.println("Failed to connect");
+ 
+        // ESP.restart();
+ 
+    }
+} 
+
+  
+  // pinMode(TRIGGER_PIN, INPUT_PULLUP);
+
+  // pinMode(RC_CH2_PIN, INPUT); //rc0
+  //attachInterrupt(digitalPinToInterrupt(RC_CH2_PIN), [](){
+  //  throttle = pulseIn(RC_CH2_PIN, HIGH, 20000) / 10;
+  //}, CHANGE);
+  
+  
+  // pinMode(RC_CH6_PIN, INPUT); //rc1
+  //attachInterrupt(digitalPinToInterrupt(RC_CH6_PIN), [](){
+  //  steering = pulseIn(RC_CH6_PIN, HIGH, 20000) / 10;
+  //}, CHANGE);
+
+  pinMode(MOTOR_PIN, OUTPUT); // relay3
+  pinMode(ACTUATOR_PIN1, OUTPUT);
+  pinMode(ACTUATOR_PIN2, OUTPUT);
+  pinMode(BLADE_PIN, OUTPUT);
+  digitalWrite(MOTOR_PIN, LOW);
+  digitalWrite(BLADE_PIN, LOW);
+  digitalWrite(ACTUATOR_PIN1, LOW);
+  digitalWrite(ACTUATOR_PIN2, LOW);
+  
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(10000);
+  //   Serial.println("Connecting to WiFi...");
+  // }
+   
+ 
+
+  // Serial.println("Connected to WiFi");
+  // Serial.println("IP address: ");
+  // Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  
+  server.on("/forward", handleForward);
+  server.on("/left", handleLeft);
+  server.on("/stop", handleStop);
+  server.on("/right", handleRight);
+  server.on("/backward", handleBackward);
+  server.onNotFound(handleNotFound);
+  
+  server.on("/motor", handleMotor); //n
+  server.on("/actuator", handleActuator);//n
+
+  server.on("/bladeon", handleBladeOn);
+  server.on("/bladeoff", handleBladeOff);
+  
+  server.begin();
+}
+
+
+void loop() {
+
+
+  if (stateForBlade == 1){
+    digitalWrite(BLADE_PIN, HIGH);
+    Serial.print("OnOnOn");
+  }
+  if (stateForBlade == 0){
+    digitalWrite(BLADE_PIN, LOW);
+    Serial.print("OfOfOf");
+  }
+
+
+  delay(100);
+if ( digitalRead(TRIGGER_PIN) == LOW) {
+  bool res;
+    WiFiManager wm;    
+ 
+    //reset settings - for testing
+    //wm.resetSettings();
+  
+    // set configportal timeout
+    wm.resetSettings();
+    Serial.print("Cleaned");
+    res = wm.autoConnect("Willa_Mower","BobdaSML"); // password protected ap
+    if(!res) {
+ 
+        Serial.println("Failed to connect");
+ 
+        // ESP.restart();
+ 
+    }
+  }
+
+  if (WiFi.status() != WL_CONNECTED){
+    delay(5000);
+    Serial.print("Unconneted");
+  }
+
+
+  WiFiManager wm; 
+  String ssid;
+  String passWord;
+  ssid = wm.getWiFiSSID();
+  passWord = wm.getWiFiPass();
+
+  
+  Serial1.print(ssid);
+  Serial1.print(",");
+  Serial1.print(passWord);
+  Serial1.print(".");
+  Serial.println(ssid);
+  Serial.println(passWord);
+  // espSerial.println(ssid);
+  // espSerial.println(ssid);
+
+String receivedData = Serial1.readStringUntil(',');
+
+// if(receivedData == "192.168.168.248"){
+//   Serial.println("yes, it is equal");
+// }
+
+
+Serial.println(receivedData);
+
+htmlWithIPRemain = receivedData + ":81/stream'>";
+
+webPageHTML = html + htmlWithIP + htmlWithIPRemain + html1;
+
+Serial.println(webPageHTML);
+
+
+
+
+
+
+  server.handleClient();
+
+
+
+  // put your main code here, to run repeatedly:
+  // Serial.println(WiFi.localIP());
+  // int CH2 = pulseIn(RC_CH2_PIN, HIGH, 25000);
+  // int CH6 = pulseIn(RC_CH6_PIN, HIGH, 25000);
+
+  // Determine motor state based on RC inputs and current motorState
+  if (motorState == FORWARD) { // || (RC_CH2_PIN > RC_CH6_FORWARD_MIN && RC_CH6_PIN < RC_CH6_FORWARD_MAX)) {
+    digitalWrite(MOTOR_PIN, HIGH);
+    Serial.print("It does work forward");
+    // digitalWrite(MOTOR_DIR1_PIN, HIGH);
+    // digitalWrite(MOTOR_DIR2_PIN, LOW); // Backward is unnecessary
+    digitalWrite(ACTUATOR_PIN1, LOW); // Retract actuator when moving forward
+    digitalWrite(ACTUATOR_PIN2, LOW); // Retract actuator when moving forward
+  //} else if (motorState == BACKWARD || (CH6 > RC_CH6_BACKWARD_MIN && CH6 < RC_CH6_BACKWARD_MAX)) {
+  //  digitalWrite(MOTOR_PIN, HIGH);
+  //  digitalWrite(MOTOR_DIR1_PIN, LOW); // could use this as stop
+  //  digitalWrite(MOTOR_DIR2_PIN, HIGH);
+  //  digitalWrite(ACTUATOR_PIN1, LOW); // Retract actuator when moving forward
+  //  digitalWrite(ACTUATOR_PIN2, LOW); // Retract actuator when moving forward
+  } else if (motorState == LEFT) { // || (RC_CH2_PIN > RC_CH2_LEFT_MIN && RC_CH6_PIN < RC_CH2_LEFT_MAX)) {
+    // digitalWrite(MOTOR_PIN, HIGH);
+    // digitalWrite(MOTOR_DIR1_PIN, LOW);
+    // digitalWrite(MOTOR_DIR2_PIN, HIGH);
+    Serial.print("It does work left");
+    digitalWrite(ACTUATOR_PIN1, LOW); // Retract actuator when turning left
+    digitalWrite(ACTUATOR_PIN2, HIGH); // Retract actuator when moving forward
+    delay(1000);
+    digitalWrite(ACTUATOR_PIN2, LOW); // Retract actuator when moving forward
+  } else if (motorState == RIGHT) { //T || (RC_CH2_PIN > RC_CH2_RIGHT_MIN && RC_CH6_PIN < RC_CH2_RIGHT_MAX)) {
+    // digitalWrite(MOTOR_PIN, HIGH);
+    // digitalWrite(MOTOR_DIR1_PIN, HIGH);
+    // digitalWrite(MOTOR_DIR2_PIN, LOW);
+    Serial.print("It does work right");
+    digitalWrite(ACTUATOR_PIN1, HIGH); // Extend actuator when turning right
+    digitalWrite(ACTUATOR_PIN2, LOW); // Retract actuator when moving forward
+    delay(1000);
+    digitalWrite(ACTUATOR_PIN1, LOW); // Retract actuator when moving forward
+  } else { // if STOP
+    digitalWrite(MOTOR_PIN, LOW);
+    Serial.print("It is stop");
+    digitalWrite(ACTUATOR_PIN1, LOW); // Retract actuator when moving forward
+    digitalWrite(ACTUATOR_PIN2, LOW); // Retract actuator when moving forward
+    
+  }
+}
---
+++ Sketch_07.1_CameraWebServer.ino
@@ -0,0 +1,180 @@
+/**********************************************************************
+  Filename    : Camera Web Server
+  Description : The camera images captured by the ESP32S3 are displayed on the web page.
+  Auther      : www.freenove.com
+  Modification: 2022/10/31
+**********************************************************************/
+/**********************************************************************
+  Do NOT use GPIO 4-13, GPIO 15 -18 in this board.
+**********************************************************************/
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <HardwareSerial.h>
+
+// ===================
+// Select camera model
+// ===================
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
+//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+// ** Espressif Internal Boards **
+//#define CAMERA_MODEL_ESP32_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S3_CAM_LCD
+
+#include "camera_pins.h"
+
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+// const char* ssid     = "axp88888";
+// const char* password = "axp888888";
+
+String receivedSSID;
+String receivedPassword;
+String sendIPaddress;
+
+#define RX_PIN 42
+#define TX_PIN 41
+
+// #define TX_PIN_ForIP 17
+// #define RX_PIN_FORIP 18
+
+// HardwareSerial espSerial(13);
+
+void startCameraServer();
+
+void setup() {
+  Serial1.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);
+  Serial.begin(115200);
+  delay(5000);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+  
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  // for larger pre-allocated frame buffer.
+  if(psramFound()){
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+    config.grab_mode = CAMERA_GRAB_LATEST;
+  } else {
+    // Limit the frame size when PSRAM is not available
+    config.frame_size = FRAMESIZE_SVGA;
+    config.fb_location = CAMERA_FB_IN_DRAM;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  s->set_vflip(s, 1); // flip it back
+  s->set_brightness(s, 1); // up the brightness just a bit
+  s->set_saturation(s, 0); // lower the saturation
+
+
+  String receivedData = Serial1.readStringUntil('\n');
+    int commaIndex = receivedData.indexOf(',');
+    int dotIndex = receivedData.indexOf('.');
+
+    if (commaIndex != -1) {
+      // Extract the SSID and password from the received data
+       receivedSSID = receivedData.substring(0, commaIndex);
+       receivedPassword = receivedData.substring(commaIndex + 1,dotIndex);
+       const char* ssidChar = receivedSSID.c_str();
+      const char* passwordChar = receivedPassword.c_str();
+      Serial.print("ssid=");
+      Serial.print(ssidChar);
+      Serial.print("pwd=");
+      Serial.println(passwordChar);
+
+      // String ssid = "axp88888";
+      // String password = "axp888888";
+      // if(receivedSSID == ssid){
+      //   Serial.print("yesyesyes");
+      // }else{
+      //   Serial.print("nonono");
+      // }
+
+      // if(receivedPassword == password){
+      //   Serial.print("hhihihihihi");
+      // }else{
+      //   Serial.print("noooooo");
+      // }
+  WiFi.begin(receivedSSID, receivedPassword);
+      }
+
+  // String ssid = "axp88888";
+  //     String password = "axp888888";    
+  
+  WiFi.setSleep(false);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+
+}
+
+void loop() {
+  // Do nothing. Everything is done in another task by the web server
+  delay(1000);
+  Serial.print(Serial.read());
+
+  
+
+
+  String ip_address = WiFi.localIP().toString();
+  Serial.print("localIPString = ");
+  Serial.println(ip_address);
+  // int testNum = 192;
+  Serial1.print(ip_address);
+  // Serial.print(first_segment);
+  Serial1.print(",");
+}
